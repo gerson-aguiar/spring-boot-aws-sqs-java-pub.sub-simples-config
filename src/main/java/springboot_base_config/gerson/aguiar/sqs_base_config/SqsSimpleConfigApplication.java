9d8350ce6aa356317414363d00a6fa0412508560
= <=>
2025-02-23 15:14:45 -0300

    first commit

---
+++ src/main/java/springboot_base_config/gerson/aguiar/sqs_base_config/SqsSimpleConfigApplication.java
@@ -0,0 +1,13 @@
+package springboot_base_config.gerson.aguiar.sqs_base_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsSimpleConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsSimpleConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot_base_config/gerson/aguiar/sqs_base_config/SqsSimpleConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot_base_config.gerson.aguiar.sqs_base_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsSimpleConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
